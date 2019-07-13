9401935e57590e1b8aa33f48ee3a48084011b2e2
Takashi Tayama <takashi_tayama@dwango.co.jp>
2019-07-13 18:57:10 +0900

    compro_r107

---
+++ vjudge/compro_r107/A.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <sstream>
+#include <queue>
+#include <map>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <string>
+#include <complex>
+ 
+#include <cassert>
+#include <cstdlib>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+ 
+#define REP(i,e) for(int i=0;i<(int)(e);i++)
+#define FOR(i,b,e) for(int i=(int)(b);i<(int)(e);i++)
+#define ALL(c) (c).begin(), (c).end()
+#define EACH(it,c) for(__typeof((c).begin()) it=(c).begin();it!=(c).end();++it)
+#define RALL(c) (c).rbegin(), (c).rend()
+#define ALLA(a,n) ((a)+0), ((a)+n)
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<int> vint;
+typedef vector<long long> vll;
+typedef vector<string> vstring;
+typedef vector<double> vdouble;
+ 
+template<class T>void pp(T v,int n){ REP(i,n)cout<<v[i]<< ' ' ; cout << endl; }
+template<class T>void pp(T v){ EACH(it,v) cout << *it << ' ' ; cout << endl;  }
+template<class T>T& ls(T& a,T b){ if(b<a) a=b; return a; }
+template<class T>T& gs(T& a,T b){ if(b>a) a=b; return a; }
+inline ll to_i(const string& s){ll n;sscanf(s.c_str(),"%lld",&n);return n;}
+inline string to_s(ll n){char buf[32];sprintf(buf,"%lld",n);return string(buf);}
+
+inline void write_bool(bool b, const string &tval, const string &fval) {
+  cout << (b ? tval : fval) << endl;
+} 
+inline void YESNO(bool b) { return write_bool(b, "YES", "NO"); }
+inline void YesNo(bool b) { return write_bool(b, "Yes", "No"); }
+inline void yesno(bool b) { return write_bool(b, "yes", "no"); }
+
+string solve(vint v) {
+  if (count(ALL(v), 3)) return "None";
+  if (*min_element(ALL(v)) == 5) return "Named";
+  if (accumulate(ALL(v), 0) >= 4.5 * v.size()) return "High";
+  return "Common";
+}
+
+int main() {
+  int n;
+  while (cin >> n) {
+    vint v(n);
+    REP(i,n) cin >> v[i];
+    cout << solve(v) << endl;
+  }  
+}
